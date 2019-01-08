204120446e533e5a21ee5cb088ddf2fa33c20f28
Shrif Nada <shrif.nada@liveramp.com>
2019-01-08 02:38:32 -0800

    add comments for readability

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,13 +1,23 @@
 #! /usr/bin/env bash
 
+# TODO comment this back in
 #if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  # First, import necessary certs to sign release artifacts
+
+  # This block unencrypts GPG signing keys and imports them into the build environment so they
+  # can be used to sign artifacts produced by this build.
   # Note that if any of these commands fail, we exit so we get quick feedback from the build.
-  echo `pwd`
+
+  # First, unencrypt the certs we'll use to sign artifacts
   openssl aes-256-cbc -K $encrypted_9718d3d48d9b_key -iv $encrypted_9718d3d48d9b_iv -in $TRAVIS_BUILD_DIR/cd/codesigning.asc.enc -out $TRAVIS_BUILD_DIR/cd/codesigning.asc -d || exit 1;
+
+  # import these certs into GPG
   gpg --fast-import $TRAVIS_BUILD_DIR/cd/codesigning.asc || exit 1;
+
+  # Remove the unencrypted cert file
   rm cd/codesigning.asc || exit 1;
 
-  # run the deploy phase and sign any artifacts
+  # Run the deploy phase (which will sign any artifacts). The build will use the GPG certs we
+  # imported above
   mvn deploy -P sign,build-src-and-docs -DskipTests=true --settings cd/mvnsettings.xml
-#fi
+#fi TODO comment back in
+
