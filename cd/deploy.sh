22acde0ca4fe56adcc856051798c7474d566db60
Shrif Nada <shrif.nada@liveramp.com>
2019-01-08 15:01:13 -0800

    use env gpg cert instead of encrypted cert file

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -3,18 +3,24 @@
 # TODO comment this back in
 #if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 
+  CERT_DIR = "${TRAVIS_BUILD_DIR}/cd/codesigning.asc";
+
   # This block unencrypts GPG signing keys and imports them into the build environment so they
   # can be used to sign artifacts produced by this build.
-  # Note that if any of these commands fail, we exit so we get quick feedback from the build.
 
-  # First, unencrypt the certs we'll use to sign artifacts
-  openssl aes-256-cbc -K $encrypted_9718d3d48d9b_key -iv $encrypted_9718d3d48d9b_iv -in $TRAVIS_BUILD_DIR/cd/codesigning.asc.enc -out $TRAVIS_BUILD_DIR/cd/codesigning.asc -d || exit 1;
+  # If any of these commands fail, exit so we get quick feedback from the build.
+  set -e
+
+  # The cert is stored as base64 to prevent interpretation issues in bash, so decode it and store
+  # it to the cert directory.
+  echo "${GPG_CERT_BASE64}" | base64 --decode -i - > "${CERT_DIR}"
+
+  # Remove the unencrypted cert file inside a trap. This way, we'll always remove the cert even if
+  # if the script fails.
+  trap "rm ${CERT_DIR}" EXIT
 
   # import these certs into GPG
-  gpg --fast-import $TRAVIS_BUILD_DIR/cd/codesigning.asc || exit 1;
-
-  # Remove the unencrypted cert file
-  rm cd/codesigning.asc || exit 1;
+  gpg --fast-import "${CERT_DIR}"
 
   # Run the deploy phase (which will sign any artifacts). The build will use the GPG certs we
   # imported above
