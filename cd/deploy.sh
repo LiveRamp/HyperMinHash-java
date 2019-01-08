f6b003eaeabefc86551f1daf0b70b4afbbd0f9fe
Shrif Nada <shrif.nada@liveramp.com>
2019-01-08 00:33:50 -0800

    use absolute paths in deploy script

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -2,9 +2,11 @@
 
 #if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   # First, import necessary certs to sign release artifacts
-  openssl aes-256-cbc -K $encrypted_9718d3d48d9b_key -iv $encrypted_9718d3d48d9b_iv -in ./codesigning.asc.enc -out ./codesigning.asc -d
-  gpg --fast-import ./codesigning.asc
-  rm cd/codesigning.asc
+  # Note that if any of these commands fail, we exit so we get quick feedback from the build.
+  echo `pwd`
+  openssl aes-256-cbc -K $encrypted_9718d3d48d9b_key -iv $encrypted_9718d3d48d9b_iv -in $TRAVIS_BUILD_DIR/cd/codesigning.asc.enc -out $TRAVIS_BUILD_DIR/cd/codesigning.asc -d || exit 1;
+  gpg --fast-import $TRAVIS_BUILD_DIR/cd/codesigning.asc || exit 1;
+  rm cd/codesigning.asc || exit 1;
 
   # run the deploy phase and sign any artifacts
   mvn deploy -P sign,build-src-and-docs --settings cd/mvnsettings.xml
